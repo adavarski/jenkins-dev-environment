7b7541878df5d545a9f37f1cd3a35764021b21df
A.Davarski <42774466+adavarski@users.noreply.github.com>
2021-07-22 11:29:03 +0300

    Update docker-setup.sh

--- utils/docker-setup.sh
+++ utils/docker-setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # install dependencies 
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common git
 
 # add the gpg key for docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
